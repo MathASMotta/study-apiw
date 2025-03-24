4dae1ae49d10ef937dae939a4a3cca68beffe77b
MathASMotta <rm550352@fiap.com.br>
2025-03-24 20:57:27 -0300

    Aula 24/03

--- src/main/java/com/github/mathasmotta/study_apiw/model/Produto.java
+++ src/main/java/com/github/mathasmotta/study_apiw/model/Produto.java
@@ -1,18 +1,21 @@
-package com.github.mathasmotta.study_apiw;
+package com.github.mathasmotta.study_apiw.model;
 
 public class Produto {
     private Long id;
     private String nome;
-    
+
     public Long getId() {
         return id;
     }
+
     public void setId(Long id) {
         this.id = id;
     }
+
     public String getNome() {
         return nome;
     }
+
     public void setNome(String nome) {
         this.nome = nome;
     }
